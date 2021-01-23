38bb399b6a1180a6cd99cf6199a07fcc0ae3ebb3
Piotr Grynfelder <dev.pitek@gmail.com>
2021-01-23 19:52:54 +0000

    fixed openvino setup

--- setup_openvino.sh
+++ setup_openvino.sh
@@ -1,8 +1,15 @@
-wget https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.1/l_openvino_toolkit_runtime_raspbian_p_2021.1.110.tgz
-sudo mkdir -p /opt/intel/openvino2021
-sudo tar -xf  l_openvino_toolkit_runtime_raspbian_p_2021.1.110.tgz --strip 1 -C /opt/intel/openvino2021
+FILE=l_openvino_toolkit_runtime_raspbian_p_2021.1.110.tgz
+
+if [ -f "$FILE" ]; then
+    echo "Using downloaded $FILE"
+else
+    wget https://storage.openvinotoolkit.org/repositories/openvino/packages/2021.1/$FILE
+fi
+
+sudo mkdir -p /opt/intel/openvino_2021
+sudo tar -xf $FILE --strip 1 -C /opt/intel/openvino_2021
 sudo apt install cmake -y
-sudo ln -s /opt/intel/openvino2021 /opt/intel/openvino
+sudo ln -sfn /opt/intel/openvino_2021 /opt/intel/openvino
 source /opt/intel/openvino/bin/setupvars.sh
 sudo usermod -a -G users "$(whoami)"
 exec $SHELL
